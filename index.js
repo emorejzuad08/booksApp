2b0e622623de50bd7eeadf0ba12e082d692c289c
emorejzuad08 <97141876+emorejzuad08@users.noreply.github.com>
2023-11-27 21:38:28 +0800

    implement ejs-mate and include a boilerplate, navbar, and footer

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
+import pg from "pg";
+import ejsMate from "ejs-mate";
 
 const app = express();
 const port = 3021;
 const API_URL_SEARCH = "https://openlibrary.org/search.json?q="
 const API_URL_COVER = "https://covers.openlibrary.org/b/isbn/" // + isbnID + "-M.jpg" - to display medium size picture
 
+// initialize middlewares
+app.engine('ejs', ejsMate);
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-    res.send("Server is successfully running");
+    res.render("index.ejs");
 })
 
 app.listen(port, () => {
